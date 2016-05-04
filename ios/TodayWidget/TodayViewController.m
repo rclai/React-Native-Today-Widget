32659b97afaf500619343051f5d3e562828370ad
Richard Lai <it.rclai@gmail.com>
2016-05-04 12:10:36 -0400

    Clean up example

--- ios/TodayWidget/TodayViewController.m
+++ ios/TodayWidget/TodayViewController.m
@@ -19,8 +19,6 @@
 - (void)loadView {
   NSURL *jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.43:8081/index.ios.bundle?platform=ios&dev=false"];
   
-  // attempt to initialize an RCTRootView and use it as the root view of our
-  // plain ‘ol UIViewController
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"TodayWidget"
                                                initialProperties:nil
@@ -30,23 +28,9 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  // This is required in order for there to be space for
+  // the React Native stuff to show up
   [self setPreferredContentSize:CGSizeMake(0, 200)];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
-}
-
-- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
-  // Perform any setup necessary in order to update the view.
-    
-  // If an error is encountered, use NCUpdateResultFailed
-  // If there's no update required, use NCUpdateResultNoData
-  // If there's an update, use NCUpdateResultNewData
-
-  completionHandler(NCUpdateResultNewData);
 }
 
 @end
