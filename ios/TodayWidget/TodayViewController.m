2cb39f08e0608f4329a168d4dd11b7da5c8ad9e3
Richard Lai <it.rclai@gmail.com>
2016-05-04 12:37:45 -0400

    Add back extra stuff that was there before

--- ios/TodayWidget/TodayViewController.m
+++ ios/TodayWidget/TodayViewController.m
@@ -33,4 +33,18 @@
   [self setPreferredContentSize:CGSizeMake(0, 200)];
 }
 
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+  // Perform any setup necessary in order to update the view.
+  
+  // If an error is encountered, use NCUpdateResultFailed
+  // If there's no update required, use NCUpdateResultNoData
+  // If there's an update, use NCUpdateResultNewData
+  completionHandler(NCUpdateResultNewData);
+}
+
 @end
